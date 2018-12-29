1235c86626829a62160bd0d2074aac143bc37a7e
william-xu <1102793+william-xu@users.noreply.github.com>
2018-12-29 17:21:00 +0800

    version 0.0.1 snapshot

---
+++ src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -0,0 +1,29 @@
+package com.thoughtmechanix.licenses.clients;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.thoughtmechanix.licenses.model.Organization;
+
+@Component
+public class OrganizationRestTemplateClient {
+	private static final Logger logger = LoggerFactory.getLogger(OrganizationRestTemplateClient.class);
+	
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Organization getOrganization(String organizationId){
+        ResponseEntity<Organization> restExchange =
+                restTemplate.exchange(
+                        "http://organizationservice/v1/organizations/{organizationId}",
+                        HttpMethod.GET,
+                        null, Organization.class, organizationId);
+
+        return restExchange.getBody();
+    }
+}
